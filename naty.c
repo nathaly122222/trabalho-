9da106e886a4b82c83710bcdb8080af619496b93
Nathaly <nathaly.brogiato.silva@escola.pr.gov.br>
2026-06-17 17:38:55 -0300

    Add files via upload

---
+++ naty.c
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Mundo! Este é meu primeiro programa em C#.");
+            Console.WriteLine("Bem-vindo à Linguagem das Linguagens! 🚀");
+        }
+    }
+}
